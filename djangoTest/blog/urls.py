e37767285f2ead87c93bd85633d148a8ecb377bb
px64vk12 <px64vk12@naver.com>
2022-06-08 23:19:55 +0900

    CBV 블로그 포스트 상세 페이지

--- djangoTest/blog/urls.py
+++ djangoTest/blog/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from . import views
 
-# view에 있는 index.html을 적용
-urlpatterns = [
-    #/번호 입력하면 해당 페이지를 출력
-    path('<int:pk>/',views.single_post_page),
-    path('',views.index)
-]
+if 0:
+    from . import viewsFBV
+
+    # view에 있는 index.html을 적용
+    urlpatterns = [
+        #/번호 입력하면 해당 페이지를 출력
+        path('<int:pk>/',viewsFBV.single_post_page),
+        path('',viewsFBV.index)
+    ]
+
+else:
+    from . import viewsCBV
+
+    # view에 있는 index.html을 적용
+    urlpatterns = [
+        #/번호 입력하면 해당 페이지를 출력
+        path('<int:pk>/',viewsCBV.PostDetail.as_view()),
+        path('', viewsCBV.PostList.as_view())
+    ]
